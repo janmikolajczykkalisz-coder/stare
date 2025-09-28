36d3204cc893fc7269ad59aabb434ca48d7b1e04
janmikolajczykkalisz-coder <jan.mikolajczyk.kalisz@gmail.com>
2025-09-28 22:46:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,16 @@ from google.oauth2.service_account import Credentials
 
 @st.cache_resource
 def get_worksheet():
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+@st.cache_resource
+def get_worksheet():
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
     creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scopes)
     client = gspread.authorize(creds)
 
@@ -17,3 +26,17 @@ def get_worksheet():
         return client.open(title).worksheet(sheet_name)
 
 ws = get_worksheet()
+
+    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scopes)
+    client = gspread.authorize(creds)
+
+    sheet_name = st.secrets.get("sheet_name", "Sheet1")
+    key = st.secrets.get("spreadsheet_key")
+    if key:
+        return client.open_by_key(key).worksheet(sheet_name)
+    else:
+        title = st.secrets.get("spreadsheet_title", "Magazyn")
+        return client.open(title).worksheet(sheet_name)
+
+ws = get_worksheet()
+
